b70616d55a7d84e15bb996ee4f0002d21ec90b55
Curtis McHale <curtis@curtismchale.ca>
2024-05-15 14:46:16 -0700

    bumping version

--- wp-proud-dashboard.php
+++ wp-proud-dashboard.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Dashboard
 Plugin URI:         https://github.com/proudcity/wp-proud-dashboard
 Description:        Multisite and other dashboard enhancements
-Version:            PCD4.2024.05.08.1100
+Version:            PCD4.2024.05.15.1445
 Author:             ProudCity
 Author URI:         http://proudcity.com
 License:            Affero GPL v3
