a315da0506013cf9121cc8ad93b4c79069e2f29e
curtismchale <curtis@curtismchale.c>
2024-08-19 11:23:40 -0700

    adding the new concierge widget

--- wp-proud-dashboard.php
+++ wp-proud-dashboard.php
@@ -10,196 +10,207 @@ License:            Affero GPL v3
 */
 
 
-class Proud_Dashboard{
+class Proud_Dashboard
+{
+    private static $instance;
 
-	private static $instance;
+    /**
+     * Spins up the instance of the plugin so that we don't get many instances running at once
+     *
+     * @since 1.0
+     * @author SFNdesign, Curtis McHale
+     *
+     * @uses $instance->init()                      The main get it running function
+     */
+    public static function instance()
+    {
 
-	/**
-	 * Spins up the instance of the plugin so that we don't get many instances running at once
-	 *
-	 * @since 1.0
-	 * @author SFNdesign, Curtis McHale
-	 *
-	 * @uses $instance->init()                      The main get it running function
-	 */
-	public static function instance(){
+        if (! self::$instance) {
+            self::$instance = new Proud_Dashboard();
+            self::$instance->init();
+        }
 
-		if ( ! self::$instance ){
-			self::$instance = new Proud_Dashboard();
-			self::$instance->init();
-		}
+    } // instance
 
-	} // instance
+    /**
+     * Spins up all the actions/filters in the plugin to really get the engine running
+     *
+     * @since 1.0
+     * @author SFNdesign, Curtis McHale
+     *
+     * @uses $this->constants()                 Defines our constants
+     * @uses $this->includes()                  Gets any includes we have
+     */
+    public function init()
+    {
 
-	/**
-	 * Spins up all the actions/filters in the plugin to really get the engine running
-	 *
-	 * @since 1.0
-	 * @author SFNdesign, Curtis McHale
-	 *
-	 * @uses $this->constants()                 Defines our constants
-	 * @uses $this->includes()                  Gets any includes we have
-	 */
-	public function init(){
+        $this->constants();
+        $this->includes();
 
-		$this->constants();
-		$this->includes();
+        add_action('wp_dashboard_setup', array( $this, 'remove_dashboard_widgets' ));
+        add_filter('wp_proud_phoenix_allowed_slugs', array( $this, 'allow_new_theme_styles' ), 1);
 
-		add_action( 'wp_dashboard_setup', array( $this, 'remove_dashboard_widgets' ) );
-		add_filter( 'wp_proud_phoenix_allowed_slugs', array( $this, 'allow_new_theme_styles' ), 1 );
-
-		add_action( 'admin_enqueue_scripts', array( $this, 'deregister_scripts' ) );
+        add_action('admin_enqueue_scripts', array( $this, 'deregister_scripts' ));
 
 
-		// Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
-		register_activation_hook( __FILE__, array( $this, 'activate' ) );
-		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
-		register_uninstall_hook( __FILE__, array( __CLASS__, 'uninstall' ) );
+        // Register hooks that are fired when the plugin is activated, deactivated, and uninstalled, respectively.
+        register_activation_hook(__FILE__, array( $this, 'activate' ));
+        register_deactivation_hook(__FILE__, array( $this, 'deactivate' ));
+        register_uninstall_hook(__FILE__, array( __CLASS__, 'uninstall' ));
 
-	} // init
+    } // init
 
-	public static function deregister_scripts(){
+    public static function deregister_scripts()
+    {
 
-		// version plugin
-		$plugin_data = get_plugin_data( __FILE__ );
-		$screen = get_current_screen();
+        // version plugin
+        $plugin_data = get_plugin_data(__FILE__);
+        $screen = get_current_screen();
 
-		if ( $screen->id == 'dashboard' && ! current_user_can( 'delete_site' ) ){
-			// stops draggable options on the WP Dashboard
-			wp_deregister_script( 'postbox' );
+        if ($screen->id == 'dashboard' && ! current_user_can('delete_site')) {
+            // stops draggable options on the WP Dashboard
+            wp_deregister_script('postbox');
 
-			// styles to hide all the draggable stuff from the UI in the dashbaord
-			wp_enqueue_style( 'wp_proud_dashboard_styles', plugins_url( '/wp-proud-dashboard/dashboard-styles.css' ), '', esc_attr( $plugin_data['Version'] ), 'all');
-		}
+            // styles to hide all the draggable stuff from the UI in the dashbaord
+            wp_enqueue_style('wp_proud_dashboard_styles', plugins_url('/wp-proud-dashboard/dashboard-styles.css'), '', esc_attr($plugin_data['Version']), 'all');
+        }
 
-	}
+    }
 
-	/**
-	 * Removes the metaboxes on the WP dashboard that we don't want
-	 *
-	 * @since 2024.05.088
-	 * @author Curtis
-	 * @access public
-	 *
-	 * @uses 	remove_meta_box() 						Removes the designated metabox
-	 */
-	public static function remove_dashboard_widgets(){
+    /**
+     * Removes the metaboxes on the WP dashboard that we don't want
+     *
+     * @since 2024.05.088
+     * @author Curtis
+     * @access public
+     *
+     * @uses 	remove_meta_box() 						Removes the designated metabox
+     */
+    public static function remove_dashboard_widgets()
+    {
 
-		global $wp_meta_boxes;
+        global $wp_meta_boxes;
 
-		remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
-		remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
-		remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
-		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
-		remove_meta_box( 'wpseo-dashboard-overview', 'dashboard', 'normal' );
-		remove_meta_box( 'rg_forms_dashboard', 'dashboard', 'normal' ); //gravityforms
-		remove_meta_box( 'auth0_dashboard_widget_age', 'dashboard', 'normal' );
-		remove_meta_box( 'auth0_dashboard_widget_gender', 'dashboard', 'normal' );
-		remove_meta_box( 'auth0_dashboard_widget_income', 'dashboard', 'normal' );
-		remove_meta_box( 'auth0_dashboard_widget_signups', 'dashboard', 'normal' );
-		remove_meta_box( 'auth0_dashboard_widget_Location', 'dashboard', 'normal' );
-		remove_meta_box( 'auth0_dashboard_widget_idp', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_rediscache', 'dashboard', 'normal' );
-		remove_meta_box( 'so-dashboard-news', 'dashboard', 'normal' );
-		remove_meta_box( 'wp_mail_smtp_reports_widget_lite', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal' );
-		remove_meta_box( 'em_booking_stats', 'dashboard', 'normal' );
-		remove_meta_box( 'blc_dashboard_widget', 'dashboard', 'normal' );
-		remove_meta_box( 'wpseo-wincher-dashboard-overview', 'dashboard', 'normal' );
-		remove_meta_box( 'google_dashboard_widget', 'dashboard', 'normal' );
+        remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+        remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
+        remove_meta_box('dashboard_primary', 'dashboard', 'side');
+        remove_meta_box('dashboard_secondary', 'dashboard', 'normal');
+        remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+        remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
+        remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+        remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+        remove_meta_box('dashboard_activity', 'dashboard', 'normal');//since 3.8
+        remove_meta_box('wpseo-dashboard-overview', 'dashboard', 'normal');
+        remove_meta_box('rg_forms_dashboard', 'dashboard', 'normal'); //gravityforms
+        remove_meta_box('auth0_dashboard_widget_age', 'dashboard', 'normal');
+        remove_meta_box('auth0_dashboard_widget_gender', 'dashboard', 'normal');
+        remove_meta_box('auth0_dashboard_widget_income', 'dashboard', 'normal');
+        remove_meta_box('auth0_dashboard_widget_signups', 'dashboard', 'normal');
+        remove_meta_box('auth0_dashboard_widget_Location', 'dashboard', 'normal');
+        remove_meta_box('auth0_dashboard_widget_idp', 'dashboard', 'normal');
+        remove_meta_box('dashboard_rediscache', 'dashboard', 'normal');
+        remove_meta_box('so-dashboard-news', 'dashboard', 'normal');
+        remove_meta_box('wp_mail_smtp_reports_widget_lite', 'dashboard', 'normal');
+        remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
+        remove_meta_box('em_booking_stats', 'dashboard', 'normal');
+        remove_meta_box('blc_dashboard_widget', 'dashboard', 'normal');
+        remove_meta_box('wpseo-wincher-dashboard-overview', 'dashboard', 'normal');
+        remove_meta_box('google_dashboard_widget', 'dashboard', 'normal');
 
-/*
-		echo '<pre>';
-		print_r( $wp_meta_boxes );
-		echo '</pre>';
-*/
-	}
+        /*
+                echo '<pre>';
+                print_r( $wp_meta_boxes );
+                echo '</pre>';
+        */
+    }
 
-	/**
-	 * Hooks in with the Pheonix theme found in the PC Analytics plugin so that we have
-	 * the styles from that theme available on this page as well
-	 *
-	 * @since 2024.05.08
-	 * @author Curtis
-	 * @access public
-	 *
-	 * @param 	array 		$page_slug 				required 				Array of page slugs in the WP admin that our styles should be shown on
-	 */
-	public static function allow_new_theme_styles( $page_slug ){
+    /**
+     * Hooks in with the Pheonix theme found in the PC Analytics plugin so that we have
+     * the styles from that theme available on this page as well
+     *
+     * @since 2024.05.08
+     * @author Curtis
+     * @access public
+     *
+     * @param 	array 		$page_slug 				required 				Array of page slugs in the WP admin that our styles should be shown on
+     */
+    public static function allow_new_theme_styles($page_slug)
+    {
 
-		$page_slug[] = 'dashboard';
+        $page_slug[] = 'dashboard';
 
-/*
-	echo '<pre>page_slug';
-	print_r( $page_slug );
-	echo '</pre>';
-*/
-		return (array) $page_slug;
-	}
+        /*
+            echo '<pre>page_slug';
+            print_r( $page_slug );
+            echo '</pre>';
+        */
+        return (array) $page_slug;
+    }
 
-	/**
-	 * Gives us any constants we need in the plugin
-	 *
-	 * @since 1.0
-	 */
-	public function constants(){
+    /**
+     * Gives us any constants we need in the plugin
+     *
+     * @since 1.0
+     */
+    public function constants()
+    {
 
-		define( 'PROUD_DASHBOARD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+        define('PROUD_DASHBOARD_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
-	}
+    }
 
-	/**
-	 * Includes any externals
-	 *
-	 * @since 1.0
-	 * @author SFNdesign, Curtis McHale
-	 * @access public
-	 */
-	public function includes(){
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/news-feed.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-help-news-feed.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-training.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-plain-language.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-clean-up.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-review.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-help-center.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-support.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-payments.php' );
-		require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-accounts.php' );
-		//require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-share.php' );
-	}
+    /**
+     * Includes any externals
+     *
+     * @since 1.0
+     * @author SFNdesign, Curtis McHale
+     * @access public
+     */
+    public function includes()
+    {
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/news-feed.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-help-news-feed.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-training.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-plain-language.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-clean-up.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-review.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-help-center.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-concierge.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-support.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-payments.php');
+        require_once(PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-accounts.php');
+        //require_once( PROUD_DASHBOARD_PLUGIN_DIR . '/widget/proudcity-share.php' );
+    }
 
-	/**
-	 * Fired when plugin is activated
-	 *
-	 * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
-	 */
-	public function activate( $network_wide ){
+    /**
+     * Fired when plugin is activated
+     *
+     * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
+     */
+    public function activate($network_wide)
+    {
 
-	} // activate
+    } // activate
 
-	/**
-	 * Fired when plugin is deactivated
-	 *
-	 * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
-	 */
-	public function deactivate( $network_wide ){
+    /**
+     * Fired when plugin is deactivated
+     *
+     * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
+     */
+    public function deactivate($network_wide)
+    {
 
-	} // deactivate
+    } // deactivate
 
-	/**
-	 * Fired when plugin is uninstalled
-	 *
-	 * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
-	 */
-	public function uninstall( $network_wide ){
+    /**
+     * Fired when plugin is uninstalled
+     *
+     * @param   bool    $network_wide   TRUE if WPMU 'super admin' uses Network Activate option
+     */
+    public function uninstall($network_wide)
+    {
 
-	} // uninstall
+    } // uninstall
 
 } // Proud_Dashboard
 
